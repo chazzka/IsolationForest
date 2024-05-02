f869da259a71811fe070919053d22ddd93b0ac47
Adam Ulrich <adamo.ulrich@gmail.com>
2024-05-02 15:01:03 +0200

    left right split

--- lib/isolation_forest.ex
+++ lib/isolation_forest.ex
@@ -11,19 +11,11 @@ defmodule IsolationForest do
     create_ranges(range)
   end
 
-
-  def init_node_map [first, second] do
-    %{
-     "l" => first, 
-     "r" => second
-    }
-  end
-
   def split ranges, dim do
     dim |>
     then(fn dim -> {Enum.at(ranges,dim),dim} end) |>
     then(fn {range,dim} -> split_range(range) |>  Enum.map(&List.replace_at(ranges,dim,&1)) end) |>
-    init_node_map
+    List.to_tuple
   end
 
   def grep_by_range data, dim, range do
@@ -31,8 +23,13 @@ defmodule IsolationForest do
     Enum.group_by(& range["e"] > Enum.at(&1, dim) and Enum.at(&1, dim) > range["s"])
   end
 
-  #TODO: zacnes range,data,dimenze, udelas hash left right
-  # vezmes z leftu range, data ,dimenze a udelas left right, to same z rightu
-  # koncis az je v hashi v datech jen jeden bod
+  def left_right data, dim, dim_count, ranges do
+    {left_range, right_range} = split(ranges, dim)
+    %{true: left_data, false: right_data} = grep_by_range(data, dim, Enum.at(left_range,dim))
+    {
+    %{"range" => left_range, "data" => left_data, "dim" => Enum.random(0..dim_count-1)},
+    %{"range" => right_range, "data" => right_data, "dim" => Enum.random(0..dim_count-1)}
+    }
+  end
 
 end
--- test/isolation_forest_test.exs
+++ test/isolation_forest_test.exs
@@ -20,15 +20,24 @@ defmodule IsolationForestTest do
   test "split - ranges are split in two" do
     ranges = [%{"s"=>1, "e"=>6}, %{"s"=>6, "e"=>7}]
     
-    assert IsolationForest.split(ranges, 0) == %{
-    "l" => [%{"s"=>1, "e"=>3.5}, %{"s"=>6, "e"=>7}], 
-    "r"=> [%{"s"=>3.5, "e"=>6}, %{"s"=>6, "e"=>7}]
+    assert IsolationForest.split(ranges, 0) == {
+     [%{"s"=>1, "e"=>3.5}, %{"s"=>6, "e"=>7}], 
+     [%{"s"=>3.5, "e"=>6}, %{"s"=>6, "e"=>7}]
     }
   end
 
   test "grep by range" do
     assert IsolationForest.grep_by_range([[1,2],[3,4],[5,6]],0,%{"s" => 0, "e" => 3.5}) ==
     %{true => [[1,2],[3,4]], false => [[5,6]]}
+  end
+
+  test "left_right" do
+    :rand.seed(:exsss, {56,151,130})
+    ranges = [%{"s"=>1, "e"=>6}, %{"s"=>6, "e"=>7}]
+    assert IsolationForest.left_right([[1,2],[3,4],[5,6]],0,2,ranges) == {
+     %{"data" => [[3, 4]], "dim" => 0, "range" => [%{"e" => 3.5, "s" => 1}, %{"e" => 7, "s" => 6}]},
+     %{"data" => [[1, 2], [5, 6]], "dim" => 1, "range" => [%{"e" => 6, "s" => 3.5}, %{"e" => 7, "s" => 6}]}
+    }
   end
   
 end
